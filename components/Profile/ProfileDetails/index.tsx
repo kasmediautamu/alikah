8961c691df4565f0f8ba85c0df81d75f787f0b52
kasmediautamu <ivan.khabuyhe1@gmail.com>
2021-09-02 08:30:29 +0300

    adds illustrations for profile components

--- components/Profile/ProfileDetails/index.tsx
+++ components/Profile/ProfileDetails/index.tsx
@@ -14,6 +14,7 @@ import s from './ProfileDetails.module.scss'
  */
 const ProfileDetails = () => {
   const [view, setView] = useState('Active')
+  const [tab, setTab] = useState('MyAdverts')
   return (
     <div className={s.profile}>
       <div className={s.left}>
@@ -32,42 +33,51 @@ const ProfileDetails = () => {
           </div>
         </div>
         <div className={s.profileFeatures}>
-          <div>
+          <div onClick={() => setTab('MyAdverts')}>
             <img src="./icons/alikah-ads-list-icon.svg" alt="alikah ads myadverts icon" />
             <p data-testid="myadverts">My adverts</p>
           </div>
-          <div>
+          <div onClick={() => setTab('Feedback')}>
             <img src="./icons/alikah-ads-feedback-icon.svg" alt="alikah ads Feedback icon" />
             <p data-testid="feedback">Feedback</p>
           </div>
-          <div>
+          <div onClick={() => setTab('Performance')}>
             <img src="./icons/alikah-ads-performance-icon.svg" alt="alikah ads Performance icon" />
             <p data-testid="performance">Performance</p>
           </div>
         </div>
-        <div className={s.profileFeatureInd}>
+        <div className={s.profileFeatureInd} onClick={() => setTab('Premium')}>
           <img src="./icons/alikah-ads-premium-services-icon.svg" alt="alikah Premium services" />
           <p data-testid="premium">Premium Services</p>
         </div>
-        <div className={s.profileFeatureInd}>
+        <div className={s.profileFeatureInd} onClick={() => setTab('Followers')}>
           <img src="./icons/alikah-ads-followers-icon.svg" alt="alikah Followers" />
           <p data-testid="followers">
             Followers <span>{'1'}</span>
           </p>
         </div>
-        <div className={s.profileFeatureInd}>
+        <div className={s.profileFeatureInd} onClick={() => setTab('Faq')}>
           <img src="./icons/alikah-ads-help-icon.svg" alt="alikah frequently asked questions" />
           <p data-testid="faq">Frequently Asked Questions</p>
         </div>
       </div>
       <div className={s.right}>
-        <Activity onSetView={setView} activeView={view} />
-        {view === 'Active' && <Active />}
-        {view === 'Reviewing' && <Reviewing />}
-        {view === 'Declined' && <Declined />}
-        {view === 'Drafts' && <Drafts />}
-        {view === 'Closed' && <Closed />}
-        {view === 'All' && <All />}
+        {tab === 'MyAdverts' && (
+          <>
+            <Activity onSetView={setView} activeView={view} />
+            {view === 'Active' && <Active />}
+            {view === 'Reviewing' && <Reviewing />}
+            {view === 'Declined' && <Declined />}
+            {view === 'Drafts' && <Drafts />}
+            {view === 'Closed' && <Closed />}
+            {view === 'All' && <All />}
+          </>
+        )}
+        {tab === 'Feedback' && <>Hi from feedback</>}
+        {tab === 'Performance' && <>Hi from Performance</>}
+        {tab === 'Premium' && <>Hi from Premium</>}
+        {tab === 'Followers' && <>Hi from Followers</>}
+        {tab === 'Faq' && <>Hi from Faq</>}
       </div>
     </div>
   )
